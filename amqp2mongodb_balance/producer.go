6f674bbd651184e475bf03e0c07f81d3d9803baf
datastream <guxianjie@gmail.com>
2012-09-03 16:50:26 +0800

    revert 5cfcec48e1cbf241f75eb92bf2774c2d99766b61  1b89a50836db9af9ec550056064278576ad98c7d

--- amqp2mongodb_balance/producer.go
+++ amqp2mongodb_balance/producer.go
@@ -46,58 +46,52 @@ func (this *Producer) connect_mongodb() {
 }
 func (this *Producer) insert_record(message_chan chan *Message) {
 	this.connect_mongodb()
+	go this.handle(message_chan)
 	for {
-		select {
-		case <-this.done:
-			{
-				this.session.Refresh()
-			}
-		case msg := <-message_chan:
-			{
-				go func() {
-					if this.handle(msg) {
-						message_chan <- msg
-
-					}
-				}()
-			}
-		}
+		<-this.done
+		this.session.Refresh()
+		go this.handle(message_chan)
 	}
 }
 
-func (this *Producer) handle(msg *Message) bool {
-	session := this.session.Clone()
+func (this *Producer) handle(message_chan chan *Message) {
+	session := this.session.Copy()
 	defer session.Close()
-	var err error
-	metrics := strings.Split(strings.TrimSpace(msg.content), "\n")
-	for i := range metrics {
-		record := NewMetric(metrics[i])
-		if record != nil {
-			err = session.DB(this.dbname).C("monitor_data").Insert(record)
-			splitname := strings.Split(metrics[i], " ")
-			host := &Host{
-				Host:   record.Hostname,
-				Metric: splitname[0],
-				Ttl:    -1,
-			}
-			err = session.DB(this.dbname).C("host_metric").Insert(host)
-
-			if err != nil {
-				if err.(*mgo.LastError).Code == 11000 {
-					err = nil
-				} else {
-					log.Println("mongodb insert failed", err)
-					this.done <- err
-					break
+	for {
+		var err error
+		msg := <-message_chan
+		metrics := strings.Split(strings.TrimSpace(msg.content), "\n")
+		for i := range metrics {
+			record := NewMetric(metrics[i])
+			if record != nil {
+				err = session.DB(this.dbname).C("monitor_data").Insert(record)
+				splitname := strings.Split(metrics[i], " ")
+				host := &Host{
+					Host:   record.Hostname,
+					Metric: splitname[0],
+					Ttl:    -1,
 				}
+				err = session.DB(this.dbname).C("host_metric").Insert(host)
+
+				if err != nil {
+					if err.(*mgo.LastError).Code == 11000 {
+						err = nil
+					} else {
+						log.Println("mongodb insert failed", err)
+						this.done <- err
+						break
+					}
+				}
+			} else {
+				log.Println("metrics error", metrics[i])
 			}
-		} else {
-			log.Println("metrics error", metrics[i])
 		}
+		if err != nil {
+			go func() {
+				message_chan <- msg
+			}()
+			break
+		}
+		msg.done <- 1
 	}
-	if err != nil {
-		return false
-	}
-	msg.done <- 1
-	return true
 }

cc6e823e17667d3cabf87a5e35353bde7998178b
datastream <guxianjie@gmail.com>
2012-09-26 23:06:13 +0800

    just accept http post body, used post speed test

---
+++ post_server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+        "flag"
+        "net/http"
+	"io/ioutil"
+	"log"
+)
+
+var (
+        port       = flag.String("port", "1234", "access port")
+)
+
+func PostApi(w http.ResponseWriter, req *http.Request) {
+	_, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	w.Header().Set("Content-Type", "text/plain; charset=\"utf-8\"")
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("hello world!"))
+}
+
+func main() {
+        flag.Parse()
+        http.HandleFunc("/", PostApi)
+	err := http.ListenAndServe(":"+*port, nil)
+        if err != nil {
+                log.Println(err)
+        }
+}

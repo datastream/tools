7ba89062778667b48552160b3726691ab617ce0f
datastream <guxianjie@gmail.com>
2016-10-19 14:19:42 +0800

    pass test

--- dtdcache/main.go
+++ dtdcache/main.go
@@ -1,30 +1,31 @@
 package main
 
 import (
-        "flag"
+	"flag"
+	"fmt"
 	"github.com/gin-gonic/gin"
-        "fmt"
-        "net/http"
-        "os"
 	"io/ioutil"
-        "strings"
+	"net/http"
+	"os"
+	"strings"
 )
 
 var (
-        cachePath = flag.String("d", "./", "cache files dir")
-        listenAddr = flag.String("l", "8080", "listen addr")
+	cachePath  = flag.String("d", "./", "cache files dir")
+	listenAddr = flag.String("l", ":8080", "listen addr")
 )
+
 func main() {
 	flag.Parse()
 	r := gin.Default()
-	r.GET("/*", DownloadDtD)
-        r.Run(*listenAddr)
+	r.GET("/*filepath", DownloadDtD)
+	r.Run(*listenAddr)
 }
 
 func DownloadDtD(c *gin.Context) {
 	c.Header("Content-Type", "text/xml; charset=\"utf-8\"")
 	url := c.Request.URL.RequestURI()[1:]
-	resp, err := http.Get(fmt.Sprintf("http://%s",url))
+	resp, err := http.Get(fmt.Sprintf("http://%s", url))
 	if resp.StatusCode != 200 {
 		c.String(http.StatusServiceUnavailable, "fail to reach baclend")
 	}
@@ -33,18 +34,13 @@ func DownloadDtD(c *gin.Context) {
 		c.String(http.StatusInternalServerError, "fail to read body")
 	}
 	defer resp.Body.Close()
-	c.Header("Cache-Control","max-age=436800")
+	c.Header("Cache-Control", "max-age=436800")
 	c.String(http.StatusOK, string(body))
 	filePath := fmt.Sprintf("%s%s", *cachePath, c.Request.URL.Path)
 	paths := strings.Split(filePath, "/")
-	err = os.MkdirAll(strings.Join(paths[:len(paths)-1], "/"), os.ModeDir)
-	fp, err := os.Open(filePath)
+	err = os.MkdirAll(strings.Join(paths[:len(paths)-1], "/"), os.ModeDir|os.ModePerm)
+	err = ioutil.WriteFile(filePath, body, 0644)
 	if err != nil {
 		return
 	}
-	n, err := fp.Write(body)
-	if err != nil || len(body) != n {
-		os.Remove(filePath)
-	}
-	defer fp.Close()
 }

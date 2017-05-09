0c2a175add1650341a302bf72cb09d3d271a55cf
datastream <guxianjie@gmail.com>
2017-05-09 23:12:08 +0800

    add socks5server

---
+++ socks5server/proxy.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"io"
+	"log"
+	"math/rand"
+	"net"
+	"strconv"
+)
+
+var seed int64
+
+func rfc1918private(ip net.IP) bool {
+	for _, cidr := range []string{"10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"} {
+		_, subnet, err := net.ParseCIDR(cidr)
+		if err != nil {
+			panic("failed to parse hardcoded rfc1918 cidr: " + err.Error())
+		}
+		if subnet.Contains(ip) {
+			return true
+		}
+	}
+	return false
+}
+
+func getAllIPv4PublicIP() ([]net.IP, error) {
+	var IPAddresses []net.IP
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return IPAddresses, err
+	}
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			private := rfc1918private(ip)
+			if !(private || ip.IsLoopback()) && (ip.To4() != nil) {
+				IPAddresses = append(IPAddresses, ip)
+			}
+		}
+	}
+	return IPAddresses, nil
+}
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	seed = 42
+	l, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for {
+		client, err := l.Accept()
+		if err != nil {
+			log.Panic(err)
+		}
+
+		go handleClientRequest(client)
+	}
+}
+
+func handleClientRequest(client net.Conn) {
+	if client == nil {
+		return
+	}
+	defer client.Close()
+
+	var b [1024]byte
+	n, err := client.Read(b[:])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if b[0] == 0x05 {
+		client.Write([]byte{0x05, 0x00})
+		n, err = client.Read(b[:])
+		var host, port string
+		switch b[3] {
+		case 0x01: //IP V4
+			host = net.IPv4(b[4], b[5], b[6], b[7]).String()
+		case 0x03: // domain
+			host = string(b[5 : n-2])
+		case 0x04: //IP V6
+			host = net.IP{b[4], b[5], b[6], b[7], b[8], b[9], b[10], b[11], b[12], b[13], b[14], b[15], b[16], b[17], b[18], b[19]}.String()
+		}
+		port = strconv.Itoa(int(b[n-2])<<8 | int(b[n-1]))
+
+		ips, _ := getAllIPv4PublicIP()
+		rand.Seed(seed)
+		localeAddr := &net.TCPAddr{
+			IP:   ips[rand.Intn(len(ips))],
+			Port: rand.Intn(64510) + 1024,
+		}
+		seed = int64(rand.Intn(64510))
+		log.Println(localeAddr)
+		remouteAddr, err := net.ResolveTCPAddr("tcp", net.JoinHostPort(host, port))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(remouteAddr)
+		server, err := net.DialTCP("tcp", localeAddr, remouteAddr)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		client.Write([]byte{0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+		go io.Copy(client, server)
+		io.Copy(server, client)
+		server.Close()
+	}
+	log.Println("close connect")
+}
